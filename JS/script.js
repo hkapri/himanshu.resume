c08472b42dea50d07a8cb6ed9a0d90db06880053
hkapri <hkapri.hk@gmail.com>
2022-08-18 19:11:37 +0530

    [MOD] Make the design responsive added skills animation

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,6 @@
 const down =document.getElementById('arrow_down');
 const up   =document.getElementById('arrow_up');
+const bubble   =document.getElementById('bubble_view');
 
 window.onscroll=function(){
     drophide();
@@ -10,10 +11,12 @@ function drophide()
 if(document.body.scrollTop > 30 || document.documentElement.scrollTop > 30)
 {
     down.style.display='none';
+    bubble.style.display='none';
 }
 else
 {
     down.style.display="block";
+    bubble.style.display="block";
 }
 }
 
